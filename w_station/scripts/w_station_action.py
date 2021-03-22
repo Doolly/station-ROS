6a1c5b4a72795ccb9d3e03c1258b60f12ff7c32b
hyuncello <jeremykim95@gmail.com>
2021-03-22 16:49:04 +0900

    edit main code

--- w_station/scripts/w_station_action.py
+++ w_station/scripts/w_station_action.py
@@ -2,96 +2,38 @@
 #_*_coding:utf-8_*_
 
 import rospy
-from std_msgs.msg import Int8MultiArray, Int8, String, Bool
+from std_msgs.msg import Int8, String, Bool
 
-global floor
-global state
-global destination
-
-
-
-class ItemStatusSubscriber():
+class LiftDestinationFloorPublisher():
     def __init__(self):
-        self.item_status_sub = rospy.Subscriber("wstation/item_status", Int8MultiArray, callback = self._callback)
-        self.item_status_buf = None
+        self.lift_destination_floor_pub = rospy.Publisher("wstation/lift_destination_floor", Int8, queue_size=1)
+        self.msg = Int8()
 
-        self.secondFloorCount = 0
-        self.thirdFloorCount = 0
-
-    def function(self):
-        # priority => high floor                                                    
-        if (self.secondFloorCount > self.thirdFloorCount):
-            return 2
-        elif (self.secondFloorCount < self.thirdFloorCount):
-            return 3
-        elif (self.secondFloorCount == 0 and self.thirdFloorCount == 0):
-            return -1
-        else:
-            # same count
-            return 3
-    
-    def _callback(self, msg):
-        self.secondFloorCount = 0
-        self.thirdFloorCount = 0
-
-        matrix = [[0 for msg.layout.dim[0].stride in range(msg.layout.dim[0].size)] for msg.layout.dim[1].stride in range(msg.layout.dim[1].size)]
-
-        for i in range(msg.layout.dim[1].size):
-            for j in range(msg.layout.dim[0].size):
-                matrix[i][j] = msg.data[(i * msg.layout.dim[1].size) + j]
-
-        for i in range(msg.layout.dim[1].size):
-            if (matrix[1][i] == 1):
-                self.secondFloorCount += 1
-            if (matrix[2][i] == 1):
-                self.thirdFloorCount += 1
+    def send_lift_destination_floor(self,floor):
+        self.msg.data = floor
+        self.lift_destination_floor_pub.publish(self.msg)
 
 
-class LiftCurrentFloorSubscriber():
+class PushItemToLiftPublisher():
     def __init__(self):
-        self.lift_current_floor_sub = rospy.Subscriber("wstation/lift_current_floor", Int8, callback=self._callback)
-        self.lift_current_floor_buf = None
+        self.push_item_to_lift_pub = rospy.Publisher("wstation/push_item_to_lift", Bool, queue_size=1)
+        self.msg = Bool()
 
-    def function(self):
-        return self.lift_current_floor_buf
-    
-    def _callback(self, msg):
-        self.lift_current_floor_buf = msg.data
+    def send_push_item_to_lift(self, push):
+        self.msg.data = push
+        self.push_item_to_lift_pub.publish(self.msg)
 
 
-class LiftStatusSubscriber():
+class SendToDestinationPublisher():
     def __init__(self):
-        self.lift_status_sub = rospy.Subscriber("wstation/lift_status", String, callback=self._callback)
-        self.lift_status_buf = None
+        self.send_to_destination_pub = rospy.Publisher("wstation/send_to_destination", String, queue_size=1)
+        self.msg = String()
 
-    def function(self):
-        return self.lift_status_buf
+    def send_to_destination(self, message):
+        self.msg.data = message
+        print("go to "+message+" is published")
+        self.send_to_destination_pub.publish(self.msg)
 
-    def _callback(self, msg):
-        self.lift_status_buf = msg.data
-
-class LiftItemStatusSubscriber():
-    def __init__(self):
-        self.lift_item_status_sub = rospy.Subscriber("wstation/lift_item_status", String, callback=self._callback)
-        self.lift_item_status_buf = None
-
-    def function(self):
-        return self.lift_item_status_buf
-    
-    def _callback(self, msg):
-        self.lift_item_status_buf = msg.data
-
-
-class LiftItemSizeSubscriber():
-    def __init__(self):
-        self.lift_item_size_sub = rospy.Subscriber("wstation/lift_item_size", String, callback=self._callback)
-        self.lift_item_size_buf = None
-
-    def function(self):        
-        return self.lift_item_size_buf
-    
-    def _callback(self, msg):
-        self.lift_item_size_buf = msg.data
 
 
 ######################################################################################################################
@@ -99,145 +41,88 @@ class LiftItemSizeSubscriber():
 
 class TopicList:
 
-    flooritemstatus = -1           # array, first 4 is 1st floor, and so on
-    liftcurrentfloor = 0           # 1, 2, 3
-    liftstatus = "arrived"         # arrived, up, down
-    liftitemstatus = False         # True, False
-    liftitemsize = "none"          # good, bad, none
-    jamesarrived = False           # True, False # For identify James is Exist or not
-    status = "none"                # 
-
-class WstationTask(TopicList):
-    def __init__(self):
-        print("hi")
+    liftdestinationfloor = -1      # -1, 1, 2, 3
+    sendtodestination = "none"     # james, tray, none
+    pushitem = False               # True, False
+    status = "none"
     
-    # def get_topic(self,data1,data2,data3,data4,data5,data6,data7,data8,data9):
-    
-    def definestatus(self):
-        if emergency != True:
-            if (jamesarrived == False) and (liftitemstatus == True) and (liftitemstatus == "good") and (liftcurrentfloor == 1):
-                status = "waitjames"
-                return status
 
-            elif (jamesarrived == True) and (liftitemstatus == True) and (liftitemstatus == "good") and (liftcurrentfloor == 1):
-                status = "tojames"
-                return status
-
-            elif (jamesarrived == True) and (liftitemstatus == True) and (liftitemstatus == "bad") and (liftcurrentfloor == 1):
-                status = "totray"
-                return status
-
-            elif (liftitemstatus == True) and (liftcurrentfloor != 1):
-                status = "gofirstfloor"
-                return status
-
-            elif (liftitemstatus == False) and (liftcurrentfloor == 1) and (flooritemstatus == 2):
-                status = "gosecondfloor"
-                return status
-
-            elif (liftitemstatus == False) and (liftcurrentfloor == 1) and (flooritemstatus == 3):
-                status = "gothirdfloor"
-                return status
-                
-            elif (liftitemstatus == False) and (liftcurrentfloor = flooritemstatus):
-                status = "pushitem"
-                return status
-
-class WstationStatusPublisher(TopicList):
+class WstationStatusSubscriber(TopicList):
     def __init__(self):
-        self.send_to_destination_pub = rospy.Publisher("wstation/status", String, queue_size=1)
-        self.msg = String()
+        self.wstation_status_sub = rospy.Subscriber("wstation/status", String, callback=self._callback)
+        self.wstation_status_buf = self.status
 
-    def send_wstation_status(self):
-        self.msg.data = status
-        print("Status :  " + message)
-        self.send_to_destination_pub.publish(self.msg)
+    # waitjames,tojames,totray,gofirstfloor,gosecondfloor,gothirdfloor,pushitem
+    def function(self):
+        if self.status == "waitjames":
+            liftdestinationfloor = -1
+            sendtodestination = "none"
+            pushitem = False 
+
+        if self.status == "tojames":
+            liftdestinationfloor = -1
+            sendtodestination = "none"
+            pushitem = False
+
+        if self.status == "totray":
+            liftdestinationfloor = -1
+            sendtodestination = "none"
+            pushitem = False 
+
+        if self.status == "gofirstfloor":
+            liftdestinationfloor = -1
+            sendtodestination = "none"
+            pushitem = False 
+
+        if self.status == "gosecondfloor":
+            liftdestinationfloor = -1
+            sendtodestination = "none"
+            pushitem = False 
+
+        if self.status == "gothirdfloor":
+            liftdestinationfloor = -1
+            sendtodestination = "none"
+            pushitem = False 
+
+        if self.status == "pushitem":
+            liftdestinationfloor = -1
+            sendtodestination = "none"
+            pushitem = False 
+    
+    def _callback(self, msg):
+        self.wstation_status_buf = msg.data
+
+    # def is_item_push_into_lift(self):
+
+    # def is_item_push_into_lift(self):
+        
 
 ####################################################################################################################
 ####################################################################################################################
 
-
+        
 def wstation_main():
-    rospy.init_node('Wstation_sub',anonymous=False)
-    rate = rospy.Rate(10) # 300hz?
 
-    item_status_sub            = ItemStatusSubscriber()             # IrSensor list
-    lift_current_floor_sub     = LiftCurrentFloorSubscriber()
-    lift_status_sub            = LiftStatusSubscriber()
-    lift_item_status_sub       = LiftItemStatusSubscriber()
-    lift_item_size_sub         = LiftItemSizeSubscriber()
+    rospy.init_node('Wstation_pub',anonymous=False)
+    rate = rospy.Rate(4) # 300hz?
 
-    wstation_status            = WstationStatusPublisher()
+    lift_destination_floor_pub = LiftDestinationFloorPublisher()    # -1, 1, 2, 3
+    send_to_destination_pub    = SendToDestinationPublisher()       # "james", "tray", "none"
+    push_item_to_lift_pub      = PushItemToLiftPublisher()          # "push" ,"none"
+    wstation_status_sub        = WstationStatusSubscriber()
+
 
     print("Wstation start")
     while not rospy.is_shutdown():
 
         rate.sleep()
 
+        wstation_status_sub.function()
 
-        # Subscribe from Arduino
-        TopicList.itemstatus = item_status_sub.function()
-        TopicList.liftcurrentfloor = lift_current_floor_sub.function()            # 1, 2, 3
-        TopicList.liftstatus = lift_status_sub.function()                     # "wait", "move", "arrived"
-        TopicList.liftitemstatus = lift_item_status_sub.function()            # "none", "exist"
-        TopicList.liftitemsize = lift_item_size_sub.function()            # "good", "bad"
-
-
-
-        WstationTask.definestatus()
-
-        wstation_status.send_wstation_status()
-
-        # if (liftStatus == "wait"):
-        #     # print(TopicList.pushitem)
-        #     TopicList.pushitem = True
-            # print(TopicList.pushitem)
-
-        # # ros wstation process
-        # if (liftStatus == "wait"):
-        #     print("Stage1 => start!")
-        #     destinationFloor = item_status_sub.function()
-
-        #     if (destinationFloor != -1):
-        #         print("item is not exist")
-
-        # elif (liftStatus == "arrived" and liftItemStatus == "none"):
-        #     print("State2 => Push Item")
-
-        #     destinationFloor = -1
-        #     pushCommand = "push"
-            
-        #     print("State2 => push item to lift")
-
-        # elif (liftStatus == "arrived" and liftItemStatus == "exist" and itemSizeStatus != "good" and itemSizeStatus != "bad"):
-        #     print("State3 => Camera Check!")
-
-        #     pushCommand = "none"
-
-        #     # ros 에서 우선 send_to_destination 메세지를 주기 전에 1층으로 움직이라는 명령을 주어야 함
-        #     if (currentFloor != 1):
-        #         destinationFloor = 1
-        #         continue
-        #     else:
-        #         destinationFloor = -1
-        #     # item size check needed!
-        #     # itemSizeStatus = lift_item_size_sub.function()
-        #     # itemSizeStatus = "bad"
-
-            
-
-        # if (itemSizeStatus == "good"):
-        #     print("State4 => Send to James")
-        
-        #     if (currentFloor == 1):
-        #         send_to_destination_pub.send_to_destination("james")
-
-
-        # elif (itemSizeStatus == "bad"):
-        #     print("State5 => Send to Tray")
-
-        #     if (currentFloor == 1):
-        #         send_to_destination_pub.send_to_destination("tray")
+        # Publish
+        lift_destination_floor_pub.send_lift_destination_floor(TopicList.liftdestinationfloor)
+        send_to_destination_pub.send_to_destination(TopicList.sendtodestination)
+        push_item_to_lift_pub.send_push_item_to_lift(TopicList.pushitem)
 
 
 if __name__ == '__main__':
--- w_station/scripts/w_station_status.py
+++ w_station/scripts/w_station_status.py
@@ -2,38 +2,107 @@
 #_*_coding:utf-8_*_
 
 import rospy
-from std_msgs.msg import Int8, String, Bool
+from std_msgs.msg import Int8MultiArray, Int8, String, Bool
 
-class LiftDestinationFloorPublisher():
+global floor
+global state
+global destination
+
+
+
+class ItemStatusSubscriber():
     def __init__(self):
-        self.lift_destination_floor_pub = rospy.Publisher("wstation/lift_destination_floor", Int8, queue_size=1)
-        self.msg = Int8()
+        self.item_status_sub = rospy.Subscriber("wstation/item_status", Int8MultiArray, callback = self._callback)
+        self.item_status_buf = None
 
-    def send_lift_destination_floor(self,floor):
-        self.msg.data = floor
-        self.lift_destination_floor_pub.publish(self.msg)
+        self.secondFloorCount = 0
+        self.thirdFloorCount = 0
+
+    def function(self):
+        # priority => high floor                                                    
+        if (self.secondFloorCount > self.thirdFloorCount):
+            return 2
+        elif (self.secondFloorCount < self.thirdFloorCount):
+            return 3
+        elif (self.secondFloorCount == 0 and self.thirdFloorCount == 0):
+            return -1
+        else:
+            # same count
+            return 3
+    
+    def _callback(self, msg):
+        self.secondFloorCount = 0
+        self.thirdFloorCount = 0
+
+        matrix = [[0 for msg.layout.dim[0].stride in range(msg.layout.dim[0].size)] for msg.layout.dim[1].stride in range(msg.layout.dim[1].size)]
+
+        for i in range(msg.layout.dim[1].size):
+            for j in range(msg.layout.dim[0].size):
+                matrix[i][j] = msg.data[(i * msg.layout.dim[1].size) + j]
+
+        for i in range(msg.layout.dim[1].size):
+            if (matrix[1][i] == 1):
+                self.secondFloorCount += 1
+            if (matrix[2][i] == 1):
+                self.thirdFloorCount += 1
 
 
-class PushItemToLiftPublisher():
+class LiftCurrentFloorSubscriber():
     def __init__(self):
-        self.push_item_to_lift_pub = rospy.Publisher("wstation/push_item_to_lift", Bool, queue_size=1)
-        self.msg = Bool()
+        self.lift_current_floor_sub = rospy.Subscriber("wstation/lift_current_floor", Int8, callback=self._callback)
+        self.lift_current_floor_buf = None
 
-    def send_push_item_to_lift(self, push):
-        self.msg.data = push
-        self.push_item_to_lift_pub.publish(self.msg)
+    def function(self):
+        return self.lift_current_floor_buf
+    
+    def _callback(self, msg):
+        self.lift_current_floor_buf = msg.data
 
 
-class SendToDestinationPublisher():
+class LiftStatusSubscriber():
     def __init__(self):
-        self.send_to_destination_pub = rospy.Publisher("wstation/send_to_destination", String, queue_size=1)
-        self.msg = String()
+        self.lift_status_sub = rospy.Subscriber("wstation/lift_status", String, callback=self._callback)
+        self.lift_status_buf = None
 
-    def send_to_destination(self, message):
-        self.msg.data = message
-        print("go to "+message+" is published")
-        self.send_to_destination_pub.publish(self.msg)
+    def function(self):
+        return self.lift_status_buf
 
+    def _callback(self, msg):
+        self.lift_status_buf = msg.data
+
+class LiftItemStatusSubscriber():
+    def __init__(self):
+        self.lift_item_status_sub = rospy.Subscriber("wstation/lift_item_status", String, callback=self._callback)
+        self.lift_item_status_buf = None
+
+    def function(self):
+        return self.lift_item_status_buf
+    
+    def _callback(self, msg):
+        self.lift_item_status_buf = msg.data
+
+
+class LiftItemSizeSubscriber():
+    def __init__(self):
+        self.lift_item_size_sub = rospy.Subscriber("wstation/lift_item_size", String, callback=self._callback)
+        self.lift_item_size_buf = None
+
+    def function(self):        
+        return self.lift_item_size_buf
+    
+    def _callback(self, msg):
+        self.lift_item_size_buf = msg.data
+
+class EmergencySubscriber():
+    def __init__(self):
+        self.emergency_sub = rospy.Subscriber("wstation/emergency", Bool, callback=self._callback)
+        self.emergency_buf = False
+
+    def function(self):        
+        return self.emergency_buf
+    
+    def _callback(self, msg):
+        self.emergency_buf = msg.data
 
 
 ######################################################################################################################
@@ -41,139 +110,99 @@ class SendToDestinationPublisher():
 
 class TopicList:
 
-    liftdestinationfloor = -1      # -1, 1, 2, 3
-    sendtodestination = "none"     # james, tray, none
-    pushitem = False               # True, False
-    
+    flooritemstatus = -1           # array, first 4 is 1st floor, and so on
+    liftcurrentfloor = 0           # 1, 2, 3
+    liftstatus = "arrived"         # arrived, up, down
+    liftitemstatus = False         # True, False
+    liftitemsize = "none"          # good, bad, none
+    jamesarrived = False           # True, False # For identify James is Exist or not
+    status = "none"                # 
+    emergency = False
 
-class WstationStatusSubscriber(TopicList):
+
+class WstationTask(TopicList):
     def __init__(self):
-        self.wstation_status_sub = rospy.Subscriber("wstation/status", String, callback=self._callback)
-        self.wstation_status_pub = None
-
-    # waitjames,tojames,totray,gofirstfloor,gosecondfloor,gothirdfloor,pushitem
-    def function(self):
-        if status = "waitjames":
-            liftdestinationfloor = -1
-            sendtodestination = "none"
-            pushitem = False 
-
-        if status = "tojames":
-            liftdestinationfloor = -1
-            sendtodestination = "none"
-            pushitem = False
-
-        if status = "totray":
-            liftdestinationfloor = -1
-            sendtodestination = "none"
-            pushitem = False 
-
-        if status = "gofirstfloor":
-            liftdestinationfloor = -1
-            sendtodestination = "none"
-            pushitem = False 
-
-        if status = "gosecondfloor":
-            liftdestinationfloor = -1
-            sendtodestination = "none"
-            pushitem = False 
-
-        if status = "gothirdfloor":
-            liftdestinationfloor = -1
-            sendtodestination = "none"
-            pushitem = False 
-
-        if status = "pushitem":
-            liftdestinationfloor = -1
-            sendtodestination = "none"
-            pushitem = False 
+        print("hi")
+        
+    # def get_topic(self,data1,data2,data3,data4,data5,data6,data7,data8,data9):
     
-    def _callback(self, msg):
-        self.lift_current_floor_buf = msg.data
+    def definestatus(self):
+        if self.emergency != True:
+            if (self.jamesarrived == False) and (self.liftitemstatus == True) and (self.liftitemstatus == "good") and (self.liftcurrentfloor == 1):
+                status = "waitjames"
+                return status
 
-    # def is_item_push_into_lift(self):
+            elif (self.jamesarrived == True) and (self.liftitemstatus == True) and (self.liftitemstatus == "good") and (self.liftcurrentfloor == 1):
+                self.status = "tojames"
+                return status
 
-    # def is_item_push_into_lift(self):
-        
+            elif (self.jamesarrived == True) and (self.liftitemstatus == True) and (self.liftitemstatus == "bad") and (self.liftcurrentfloor == 1):
+                self.status = "totray"
+                return status
+
+            elif (self.liftitemstatus == True) and (self.liftcurrentfloor != 1):
+                self.status = "gofirstfloor"
+                return status
+
+            elif (self.liftitemstatus == False) and (self.liftcurrentfloor == 1) and (self.flooritemstatus == 2):
+                self.status = "gosecondfloor"
+                return status
+
+            elif (self.liftitemstatus == False) and (self.liftcurrentfloor == 1) and (self.flooritemstatus == 3):
+                self.status = "gothirdfloor"
+                return status
+                
+            elif (self.liftitemstatus == False) and (self.liftcurrentfloor == self.flooritemstatus):
+                self.status = "pushitem"
+                return status
+
+class WstationStatusPublisher(TopicList):
+    def __init__(self):
+        self.send_to_destination_pub = rospy.Publisher("wstation/status", String, queue_size=1)
+        self.msg = String()
+
+    def send_wstation_status(self):
+        self.msg.data = self.status
+        print("Status :  " + self.msg.data)
+        self.send_to_destination_pub.publish(self.msg)
 
 ####################################################################################################################
 ####################################################################################################################
 
-        
+
 def wstation_main():
+    rospy.init_node('Wstation_sub',anonymous=False)
+    rate = rospy.Rate(10) # 300hz?
 
-    rospy.init_node('Wstation_pub',anonymous=False)
-    rate = rospy.Rate(4) # 300hz?
+    item_status_sub            = ItemStatusSubscriber()             # IrSensor list
+    lift_current_floor_sub     = LiftCurrentFloorSubscriber()
+    lift_status_sub            = LiftStatusSubscriber()
+    lift_item_status_sub       = LiftItemStatusSubscriber()
+    lift_item_size_sub         = LiftItemSizeSubscriber()
+    emergency_sub              = EmergencySubscriber()
 
-    lift_destination_floor_pub = LiftDestinationFloorPublisher()    # -1, 1, 2, 3
-    send_to_destination_pub    = SendToDestinationPublisher()       # "james", "tray", "none"
-    push_item_to_lift_pub      = PushItemToLiftPublisher()          # "push" ,"none"
-    wstation_status_sub        = WstationStatusSubscriber()
 
+    wstation_task              = WstationTask()
+    wstation_status            = WstationStatusPublisher()
 
     print("Wstation start")
     while not rospy.is_shutdown():
 
         rate.sleep()
 
-        wstation_status_sub.function()
 
-        # Publish
-        lift_destination_floor_pub.send_lift_destination_floor(TopicList.liftdestinationfloor)
-        send_to_destination_pub.send_to_destination(TopicList.sendtodestination)
-        push_item_to_lift_pub.send_push_item_to_lift(TopicList.pushitem)
-
-
-        # if (liftStatus == "wait"):
-        #     # print(TopicList.pushitem)
-        #     TopicList.pushitem = True
-            # print(TopicList.pushitem)
-
-        # # ros wstation process
-        # if (liftStatus == "wait"):
-        #     print("Stage1 => start!")
-        #     destinationFloor = item_status_sub.function()
-
-        #     if (destinationFloor != -1):
-        #         print("item is not exist")
-
-        # elif (liftStatus == "arrived" and liftItemStatus == "none"):
-        #     print("State2 => Push Item")
-
-        #     destinationFloor = -1
-        #     pushCommand = "push"
-            
-        #     print("State2 => push item to lift")
-
-        # elif (liftStatus == "arrived" and liftItemStatus == "exist" and itemSizeStatus != "good" and itemSizeStatus != "bad"):
-        #     print("State3 => Camera Check!")
-
-        #     pushCommand = "none"
-
-        #     # ros 에서 우선 send_to_destination 메세지를 주기 전에 1층으로 움직이라는 명령을 주어야 함
-        #     if (currentFloor != 1):
-        #         destinationFloor = 1
-        #         continue
-        #     else:
-        #         destinationFloor = -1
-        #     # item size check needed!
-        #     # itemSizeStatus = lift_item_size_sub.function()
-        #     # itemSizeStatus = "bad"
-
-            
-
-        # if (itemSizeStatus == "good"):
-        #     print("State4 => Send to James")
-        
-        #     if (currentFloor == 1):
-        #         send_to_destination_pub.send_to_destination("james")
+        # Subscribe from Arduino
+        TopicList.itemstatus = item_status_sub.function()
+        TopicList.liftcurrentfloor = lift_current_floor_sub.function()            # 1, 2, 3
+        TopicList.liftstatus = lift_status_sub.function()                     # "wait", "move", "arrived"
+        TopicList.liftitemstatus = lift_item_status_sub.function()            # "none", "exist"
+        TopicList.liftitemsize = lift_item_size_sub.function()            # "good", "bad"
+        TopicList.emergency = emergency_sub.function()
 
 
-        # elif (itemSizeStatus == "bad"):
-        #     print("State5 => Send to Tray")
+        wstation_task.definestatus()
 
-        #     if (currentFloor == 1):
-        #         send_to_destination_pub.send_to_destination("tray")
+        wstation_status.send_wstation_status()
 
 
 if __name__ == '__main__':
